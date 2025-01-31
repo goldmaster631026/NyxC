9ca1ab59b43580c3a94df3943859959547e3e9d0
goldmaster631026 <goldmaster631026@gmail.com>
2025-01-31 05:27:15 -0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
             <Route path='/websites' element={<Websites />} />
             <Route path='/subscription' element={<Subscription />} />
             <Route path='/more' element={<More />} />
-            {/* <Route path='/more' element={<More />} /> */}
+            {/* <Route path= element={<More />} /> */}
           </Routes>
         </Layout>
       </SharedStateProvider>
